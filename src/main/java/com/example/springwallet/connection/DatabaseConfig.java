ba84d7981fd5b97ad0ceae2541d8c148096c1ee1
= <=>
2024-01-29 21:46:54 +0300

    Feat: Test ping-pong succesfull and added the connection DB

---
+++ src/main/java/com/example/springwallet/connection/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package com.example.springwallet.connection;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+@Component
+public class DatabaseConfig {
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                Config.URL,
+                Config.USERNAME,
+                Config.PASSWORD
+        );
+    }
+}
---
+++ src/test/java/com/example/springwallet/SpringWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
